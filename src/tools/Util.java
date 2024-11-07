b8ee4e85017a30529d85f8623dd84d4932efef54
ailadesiree <aila.dauzacker@ifms.edu.br>
2024-11-07 10:20:26 -0400

    Primeiro commit do projeto de LP5

---
+++ src/tools/Util.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tools;
+
+/**
+ *
+ * @author Aila Desiree
+ */
+public class Util {
+    
+    public static void habilitar(jTextField...comp){
+        
+    }
+    
+}
